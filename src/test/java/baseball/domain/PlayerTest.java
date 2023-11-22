bae777d8bc20711b14c71e11f7f463f24c75a3a0
PUNGZI <ashyoo1367@gmail.com>
2023-11-23 01:50:07 +0900

    test(PlayerTest): 플레이어의 숫자를 정상적으로 출력하는지 테스트

---
+++ src/test/java/baseball/domain/PlayerTest.java
@@ -0,0 +1,18 @@
+package baseball.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import baseball.common.util.Utils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class PlayerTest {
+    @DisplayName("플레이어의 숫자를 정상적으로 리턴하는가")
+    @ValueSource(strings = {"123", "456", "789"})
+    @ParameterizedTest
+    void outputPlayerNumberListTest(String inputPlayerNumber) {
+        Player player = new Player(inputPlayerNumber);
+        assertThat(player.getPlayerNumber()).isEqualTo(Utils.putInList(inputPlayerNumber));
+    }
+}
