3ae20701ca96e3f56b3677fc8fcbcbe505f6f98a
PUNGZI <ashyoo1367@gmail.com>
2023-11-22 23:29:33 +0900

    feat(Computer): 컴퓨터 숫자를 랜덤으로 지정하는 기능

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,32 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private List<Integer> computerNumber;
+
+    public Computer() {
+        this.computerNumber = generateComputerNumber();
+    }
+
+    public List<Integer> getComputerNumber() {
+        return computerNumber;
+    }
+
+    private List<Integer> generateComputerNumber() {
+        return notOverlapRandomNumbers(3, 1, 9);
+    }
+
+    private List<Integer> notOverlapRandomNumbers(int numbersCount, int minNumber, int maxNumber) {
+        List<Integer> computerNumbers = new ArrayList<>();
+        while (computerNumbers.size() < numbersCount) {
+            int randomNumber = Randoms.pickNumberInRange(minNumber, maxNumber);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
+        return computerNumbers;
+    }
+}
