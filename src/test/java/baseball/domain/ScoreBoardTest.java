63f46bc20a8dafde71459ae3e64b050c3e2d93c6
PUNGZI <ashyoo1367@gmail.com>
2023-11-23 22:04:36 +0900

    test(ScoreBoardTest): 볼-스트라이크 스코어를 정상적으로 리턴하는지 테스트

--- src/test/java/baseball/domain/ScoreBoardTest.java
+++ src/test/java/baseball/domain/ScoreBoardTest.java
@@ -1,15 +1,15 @@
 package baseball.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class ScoreBoardTest {
-    @DisplayName("플레이어의 숫자를 정상적으로 리턴하는가")
-    @Test
-    void outputPlayerNumberListTest() {
-        System.out.println(ScoreBoard.getScore(2, 0));
-        System.out.println(ScoreBoard.getScore(2, 1));
-        System.out.println(ScoreBoard.getScore(0, 3));
-        System.out.println(ScoreBoard.getScore(0, 0));
+    @DisplayName("볼-스트라이크 스코어를 정상적으로 리턴하는가")
+    @CsvSource(value = {"2, 0, 2볼", "2, 1, 2볼 1스트라이크", "0, 3, 3스트라이크", "0, 0, 낫싱"})
+    @ParameterizedTest
+    void outputScoreTest(String ball, String strike, String score) {
+        Assertions.assertThat(ScoreBoard.getScore(Integer.parseInt(ball), Integer.parseInt(strike))).isEqualTo(score);
     }
 }
