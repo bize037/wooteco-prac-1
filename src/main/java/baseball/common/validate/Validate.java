6eaf5af916c0d97efb662b518669636c54b4bb38
PUNGZI <ashyoo1367@gmail.com>
2023-11-23 21:34:43 +0900

    refactor(Validate): 매직넘버 상수화

--- src/main/java/baseball/common/validate/Validate.java
+++ src/main/java/baseball/common/validate/Validate.java
@@ -1,6 +1,12 @@
 package baseball.common.validate;
 
 public class Validate {
+    private static final int DIGIT_LENGTH = 3;
+    private static final String ZERO = "0";
+    private static final String NUMBER_PATTERN = "\\d+";
+    private static final String RESTART_NUMBER = "1";
+    private static final String END_NUMBER = "2";
+
     public static void isBlank(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException();
@@ -8,25 +14,25 @@ public class Validate {
     }
 
     public static void inZero(String input) {
-        if (input.contains("0")) {
+        if (input.contains(ZERO)) {
             throw new IllegalArgumentException();
         }
     }
 
     public static void inText(String input) {
-        if (!input.matches("\\d+")) {
+        if (!input.matches(NUMBER_PATTERN)) {
             throw new IllegalArgumentException();
         }
     }
 
     public static void notThreeDigit(String input) {
-        if (input.length() != 3) {
+        if (input.length() != DIGIT_LENGTH) {
             throw new IllegalArgumentException();
         }
     }
 
     public static void notOneOrTwo(String input) {
-        if (!input.equals("1") && !input.equals("2")) {
+        if (!input.equals(RESTART_NUMBER) && !input.equals(END_NUMBER)) {
             throw new IllegalArgumentException();
         }
     }
