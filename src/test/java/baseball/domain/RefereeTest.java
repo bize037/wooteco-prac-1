2151444e7fdac3bcbcbb857b4b8c9be5a4c37cce
PUNGZI <ashyoo1367@gmail.com>
2023-11-23 21:56:29 +0900

    test(RefereeTest): 볼과 스트라이크를 계산하여 정상적으로 출력하는지 테스트

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -1,16 +1,19 @@
 package baseball.domain;
 
 import baseball.common.util.Utils;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class RefereeTest {
-    @CsvSource(value = {"143,345", "456,256", "814,814"})
+    @DisplayName("볼과 스트라이크를 계산하여 정상적으로 출력하는가")
+    @CsvSource(value = {"143,345,1,1", "456,256,0,2", "814,814,0,3"})
     @ParameterizedTest
-    void ballStrikeTest(String computerNumber, String playerNumber) {
+    void ballStrikeTest(String computerNumber, String playerNumber, String ball, String strike) {
         Referee referee = new Referee(Utils.putInList(computerNumber), Utils.putInList(playerNumber));
-        System.out.println("ball : " + referee.getBall());
-        System.out.println("strike : " + referee.getStrike());
-        System.out.println();
+        assertThat(referee.getBall()).isEqualTo(Integer.parseInt(ball));
+        assertThat(referee.getStrike()).isEqualTo(Integer.parseInt(strike));
     }
 }
