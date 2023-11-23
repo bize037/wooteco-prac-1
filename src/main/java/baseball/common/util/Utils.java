1ecd6d703f01f0d05e97018cc45f51267c3af8b7
PUNGZI <ashyoo1367@gmail.com>
2023-11-23 21:39:39 +0900

    refactor(Utils): 매직 넘버 상수화

--- src/main/java/baseball/common/util/Utils.java
+++ src/main/java/baseball/common/util/Utils.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
+    private static final int START_INDEX = 0;
+    private static final int NEXT_INDEX = 1;
+
     public static List<Integer> putInList(String inputPlayerNumber) {
         List<Integer> playerNumber = new ArrayList<>();
-        for (int i = 0; i < inputPlayerNumber.length(); i++) {
-            playerNumber.add(Integer.parseInt(inputPlayerNumber.substring(i, i + 1)));
+        for (int i = START_INDEX; i < inputPlayerNumber.length(); i++) {
+            playerNumber.add(Integer.parseInt(inputPlayerNumber.substring(i, i + NEXT_INDEX)));
         }
         return playerNumber;
     }
