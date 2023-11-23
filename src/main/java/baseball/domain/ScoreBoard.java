e1aa7df846ed9882a32f189629dfa79592e56ba0
PUNGZI <ashyoo1367@gmail.com>
2023-11-23 17:17:08 +0900

    refactor(ScoreBoard): 메서드 및 상수 이름 변경

--- src/main/java/baseball/domain/ScoreBoard.java
+++ src/main/java/baseball/domain/ScoreBoard.java
@@ -4,13 +4,13 @@ import java.util.Arrays;
 import java.util.function.BiPredicate;
 
 public enum ScoreBoard {
-    BALL_AND_STRIKE((ball, strike) -> ball > 0 && strike > 0),
-    ONLY_BALL((ball, strike) -> ball > 0 && strike == 0),
-    ONLY_STRIKE((ball, strike) -> ball == 0 && strike > 0);
+    BALL_STRIKE((ball, strike) -> ball > 0 && strike > 0),
+    BALL((ball, strike) -> ball > 0 && strike == 0),
+    STRIKE((ball, strike) -> ball == 0 && strike > 0);
 
-    private static final String BALL_AND_STRIKE_MESSAGE = "%d볼 %d스트라이크";
-    private static final String ONLY_BALL_MESSAGE = "%d볼";
-    private static final String ONLY_STRIKE_MESSAGE = "%d스트라이크";
+    private static final String BALL_STRIKE_MESSAGE = "%d볼 %d스트라이크";
+    private static final String BALL_MESSAGE = "%d볼";
+    private static final String STRIKE_MESSAGE = "%d스트라이크";
     private static final String NOTHING_MESSAGE = "낫싱";
 
     private final BiPredicate<Integer, Integer> judge;
@@ -21,21 +21,21 @@ public enum ScoreBoard {
 
     public static String getScore(int ball, int strike) {
         return Arrays.stream(values())
-                .filter(referee -> referee.judge.test(ball, strike))
+                .filter(score -> score.judge.test(ball, strike))
                 .findFirst()
-                .map(referee -> referee.getResult(ball, strike))
+                .map(score -> score.getResult(ball, strike))
                 .orElse(NOTHING_MESSAGE);
     }
 
     private String getResult(int ball, int strike) {
-        if (this == BALL_AND_STRIKE) {
-            return String.format(BALL_AND_STRIKE_MESSAGE, ball, strike);
+        if (this == BALL_STRIKE) {
+            return String.format(BALL_STRIKE_MESSAGE, ball, strike);
         }
-        if (this == ONLY_BALL) {
-            return String.format(ONLY_BALL_MESSAGE, ball);
+        if (this == BALL) {
+            return String.format(BALL_MESSAGE, ball);
         }
-        if (this == ONLY_STRIKE) {
-            return String.format(ONLY_STRIKE_MESSAGE, strike);
+        if (this == STRIKE) {
+            return String.format(STRIKE_MESSAGE, strike);
         }
         return "";
     }
